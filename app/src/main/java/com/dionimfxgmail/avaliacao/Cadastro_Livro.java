c8070385b11426868ee2564747d4f76a4663639d
dionimf <dionimf@unesc.net>
2016-11-17 15:32:09 -0200

    mais um pouco

--- app/src/main/java/com/dionimfxgmail/avaliacao/Cadastro_Livro.java
+++ app/src/main/java/com/dionimfxgmail/avaliacao/Cadastro_Livro.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
@@ -20,27 +21,29 @@ import com.dionimfxgmail.avaliacao.R;
  */
 
 public class Cadastro_Livro extends AppCompatActivity {
-    TextView ent_cadl_nome,ent_cadl_autor,ent_cadl_editora,ent_cadl_valor,ent_cadl_tipo_obra;
+    EditText ent_cadl_nome,ent_cadl_autor,ent_cadl_editora,ent_cadl_valor;
     SeekBar ent_quantidade;
+    TextView tv_cadl_qtd;
     Button bt_cadl_cadastrar;
     RadioGroup ent_radio_grupo;
     String tipomedia;
-
+    CheckBox ent_cadl_tipo_obra;
+    int qtd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tipomedia="";
         setContentView(R.layout.cadastro_livros);
-        ent_cadl_nome=(TextView) findViewById(R.id.ent_cadl_nome);
-        ent_cadl_autor=(TextView) findViewById(R.id.ent_cadl_autor);
-        ent_cadl_editora=(TextView) findViewById(R.id.ent_cadl_editora);
-        ent_cadl_valor=(TextView)findViewById(R.id.ent_cadl_valor);
+        ent_cadl_nome=(EditText) findViewById(R.id.ent_cadl_nome);
+        ent_cadl_autor=(EditText) findViewById(R.id.ent_cadl_autor);
+        ent_cadl_editora=(EditText) findViewById(R.id.ent_cadl_editora);
+        ent_cadl_valor=(EditText)findViewById(R.id.ent_cadl_valor);
         ent_quantidade=(SeekBar) findViewById(R.id.ent_quantidade);
         ent_radio_grupo=(RadioGroup)findViewById(R.id.ent_radio_grupo);
         bt_cadl_cadastrar=(Button) findViewById(R.id.bt_cadl_cadastrar);
         ent_cadl_tipo_obra=(CheckBox)findViewById(R.id.ent_cadl_tp_manga);
-
+        tv_cadl_qtd=(TextView)findViewById(R.id.tv_cadl_qtd);
 
         ent_radio_grupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
             @Override
@@ -62,7 +65,23 @@ public class Cadastro_Livro extends AppCompatActivity {
 
             }
         });
+        ent_quantidade.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                qtd=(int) Math.floor(i+1);
+                tv_cadl_qtd.setText(String.valueOf(qtd));
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
 
